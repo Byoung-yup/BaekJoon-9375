940de0c7a48af1dfd4f44dcbfbd02fbdb226f6a2
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-03-17 02:06:44 +0900

    commit

--- BaekJoon#9375/main.swift
+++ BaekJoon#9375/main.swift
@@ -4,8 +4,45 @@
 //
 //  Created by 김병엽 on 2023/03/17.
 //
+// Reference: https://velog.io/@hyoda_mon/BOJ-Swift-9375-패션왕-신해빈
 
 import Foundation
 
-print("Hello, World!")
+func solution() {
+    
+    let testCase = Int(readLine()!)!
+    
+    for _ in 0..<testCase {
+        
+        let clothesCase = Int(readLine()!)!
+        if clothesCase == 0 {
+            print(0)
+            continue
+        }
+        
+        var clothesDic: [String: [String]] = [:]
+        var clothesCount: [Int] = []
+        
+        for _ in 0..<clothesCase {
+            
+            let clothes = readLine()!.split(separator: " ").map { String($0) }
+            
+            if clothesDic.keys.contains(clothes[1]) {
+                clothesDic[clothes[1]]!.append(clothes[0])
+            } else {
+                clothesDic[clothes[1]] = [clothes[0]]
+            }
+            
+        }
+        
+        for (_, value) in clothesDic {
+            clothesCount.append(value.count + 1)
+        }
+        
+        let count = clothesCount.reduce(1, *)
+        
+        print(count - 1)
+    }
+}
 
+solution()
